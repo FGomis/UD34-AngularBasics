aa2338453ae495687a0109e33ebec8523ee75332
FGomis <fkgomis@gmail.com>
2021-09-07 21:56:51 +0200

    Tarea creada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { summaryFileName } from '@angular/compiler/src/aot/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  num1 = '';
+  num2 = '';
+  operador = '';
+  // Booleano para controlar si se ha clicado un operador o no
+  operacion = false;
+  resultado = 0;
+
+  clickNumero(n: string){
+    if(!this.operacion){
+      this.num1 = this.num1 + n;
+    } else{
+      this.num2 = this.num2 + n;
+    }
+  }
+
+  borrar(){
+    if(!this.operacion){
+      this.num1 = this.num1.slice(0, -1);
+    } else{
+      this.num2 = this.num2.slice(0, -1);
+    }
+  }
+
+  operar(op: string){
+    this.operacion = true;
+    this.operador = op;
+  }
+
+  igual(){
+    switch(this.operador){
+      case '+':
+        this.resultado = parseFloat(this.num1) + parseFloat(this.num2);
+      break;
+      case '-':
+        this.resultado = parseFloat(this.num1) - parseFloat(this.num2);
+      break;
+      case '*':
+        this.resultado = parseFloat(this.num1) * parseFloat(this.num2);
+      break;
+      case '/':
+        this.resultado = parseFloat(this.num1) / parseFloat(this.num2);
+      break;
+    }
+    this.num1 = '';
+    this.num2 = '';
+    this.operador = '';
+    this.operacion = false;
+  }
 }
+
+
